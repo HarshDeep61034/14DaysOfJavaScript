af93fbb5978573dde29e97c0f37bf4dc3eb48f24
HarshDeep61034 <hdeep61034@gmail.com>
2023-02-20 15:21:47 +0530

    made an step progress bar with html, css & js

---
+++ Step Progress Bar/script.js
@@ -0,0 +1,48 @@
+var bar = document.getElementsByClassName('progress')[0];
+var circle1 = document.getElementById('circle1');
+var circle2 = document.getElementById('circle2');
+var circle3 = document.getElementById('circle3');
+var circle4 = document.getElementById('circle4');
+var prev = document.getElementById('prev');
+
+var x = 0;
+document.getElementById('next').onclick = function(){
+    if(x <= 3){
+    x += 1;
+    }
+  
+
+
+    if(x === 1){
+    bar.style.width = "30%";
+    circle2.classList.toggle('active');
+    prev.removeAttribute('disabled');
+
+    }
+    else if(x === 2){
+        bar.style.width = "60%";
+        circle3.classList.toggle('active');
+    }
+    else if(x === 3){
+        bar.style.width = "100%";
+        circle4.classList.toggle('active');
+    }
+    
+}
+
+document.getElementById('prev').onclick = function(){
+   
+    if(x === 1){
+    bar.style.width = "0%";
+    circle2.classList.toggle('active');
+    }
+    else if(x === 2){
+        bar.style.width = "30%";
+        circle3.classList.toggle('active');
+    }
+    else if(x === 3){
+        bar.style.width = "60%";
+        circle4.classList.toggle('active');
+    }
+    x -= 1;
+}
